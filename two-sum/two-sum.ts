0761ac2b32880ed615a9a6e2291f09f02cfd0b0a
Akshay Kumar Akkinapelli <64968793+ak-akkinapelli@users.noreply.github.com>
2023-07-09 19:58:57 +0530

    Time: 66 ms (84.21%) | Memory: 45.8 MB (18.75%) - LeetSync

--- two-sum/two-sum.ts
+++ two-sum/two-sum.ts
@@ -1,13 +1,9 @@
 function twoSum(nums: number[], target: number): number[] {
-        const a =[]
-  for(let i=0;i<nums.length;i++){
-     for(let j=i+1;j< nums.length;j++){
-          if(nums[i] + nums[j] === target){
-          a.push(i)
-          a.push(j)
+        const hash ={}
+      for(let i=0;i<nums.length;i++){
+          if(target-nums[i] in hash){
+              return [hash[target-nums[i]], i]
           }
-     
-  }
-  }
-  return a
+          hash[nums[i]] = i
+      }
 };
